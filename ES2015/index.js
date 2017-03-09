65bbc09955039a6c2ba1c036d87f198918497ea1
wangYalun <326402399@qq.com>
2017-03-09 15:01:17 +0800

    文档编写

--- ES2015/index.js
+++ ES2015/index.js
@@ -8,190 +8,196 @@
 // }
 // console.log(name);//bob
 
-let name = "allen";
+(function () {
 
-while (true) {
-	let name = "bob";
-	console.log(name); //bob
-	break;
-}
-console.log(name); //allen
 
-//ex 2
-//闭包解决
-var a = [];
-for (var i = 0; i < 10; i++) {
-	a[i] = (function (i) {
-		return function () {
+	let name = "allen";
+
+	while (true) {
+		let name = "bob";
+		console.log(name); //bob
+		break;
+	}
+	console.log(name); //allen
+
+	//ex 2
+	//闭包解决
+	var a = [];
+	for (var i = 0; i < 10; i++) {
+		a[i] = (function (i) {
+			return function () {
+				console.log(i);
+			}
+		})(i);
+	}
+	a[6]();
+	//let 解决
+
+	for (let i = 0; i < 10; i++) {
+		a[i] = function () {
 			console.log(i);
 		}
-	})(i);
-}
-a[6]();
-//let 解决
-
-for (let i = 0; i < 10; i++) {
-	a[i] = function () {
-		console.log(i);
 	}
-}
-a[6]();
+	a[6]();
 
-//const也用来声明变量，但是声明的是常量。一旦声明，常量的值就不能改变。
+	//const也用来声明变量，但是声明的是常量。一旦声明，常量的值就不能改变。
 
-const PI = Math.PI;
-//const jQuery=require("jQuery");
+	const PI = Math.PI;
+	//const jQuery=require("jQuery");
 
-console.log(PI);
+	console.log(PI);
 
-//类，继承，原型，构造函数
+	//类，继承，原型，构造函数
 
-/**
-*ES5 写法
-**/
-//构造函数
-function Animal() {
-	this.type = "animal";
-}
-//原型方法
-Animal.prototype.say = function () {
-	console.log("I am " + this.type);
-}
-var animal = new Animal();
-animal.say();
-
-function Dog() {
-	this.name = "dog";
-}
-
-Dog.prototype = new Animal();
-
-//为子类添加原型方法，要放在继承之后。
-Dog.prototype.sayName = function () {
-	console.log("I am a " + this.name);
-}
-
-var dog = new Dog();
-dog.say();
-dog.sayName();
-
-//ES2015 写法
-class AnimalES2015 {
-	constructor() {
+	/**
+	*ES5 写法
+	**/
+	//构造函数
+	function Animal() {
 		this.type = "animal";
 	}
-	say() {
+	//原型方法
+	Animal.prototype.say = function () {
 		console.log("I am " + this.type);
 	}
-}
-var animal = new AnimalES2015();
-animal.say();
-class DogES2015 extends AnimalES2015 {
-	constructor() {
-		super();
+	var animal = new Animal();
+	animal.say();
+
+	function Dog() {
 		this.name = "dog";
 	}
-	sayName() {
+
+	Dog.prototype = new Animal();
+
+	//为子类添加原型方法，要放在继承之后。
+	Dog.prototype.sayName = function () {
 		console.log("I am a " + this.name);
 	}
 
-	//arrow function
-	//当我们使用箭头函数时，函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象。
-	//并不是因为箭头函数内部有绑定this的机制，实际原因是箭头函数根本没有自己的this，
-	//它的this是继承外面的，因此内部的this就是外层代码块的this。
-	intervalSayName(){
-		setInterval(()=>{
-			console.log(this.name+":"+(+new Date()));
-		},1000);
-	}
-}
+	var dog = new Dog();
+	dog.say();
+	dog.sayName();
 
-var dog = new DogES2015();
-dog.say();
-dog.sayName();
-
-//箭头函数  arrow function
-
-var fun1=function(i){ return i+1;};//ES5 
-var funArrow=(i)=>i+1;//ES2015
-
-console.log(funArrow(1));
-
-let cat="ken";
-//let dog='lili';
-let zoo={cat,dog};
-
-//Generator 函数
-
-function* helloworldGenerator(){
-	yield 'hello';
-	yield 'world';
-	return 'ending';
-}
-
-var hw=helloworldGenerator();
-
-hw.next(); //{value:'hello',done:false}
-hw.next(); //{value:'world',done:false}
-hw.next(); //{value:'ending',done:true}
-
-function* f(){
-	console.log('执行了');
-}
-
-var g=f();
-
-setInterval(function(){
-	console.log(g.next());
-},1000);
-
-
-// 研究下 类方法里的 递归
-
-class Test{
-	static getName(){
-		return "allen";
-	}
-	static staticFun(){
-		console.log(this.getName());
-	}
-	fun(){
-		console.log('TEST',this.num++);
-		if(this.num==10){
-			return ;
-		}else{
-			this.fun();
+	//ES2015 写法
+	class AnimalES2015 {
+		constructor() {
+			this.type = "animal";
+		}
+		say() {
+			console.log("I am " + this.type);
 		}
 	}
-}
+	var animal = new AnimalES2015();
+	animal.say();
+	class DogES2015 extends AnimalES2015 {
+		constructor() {
+			super();
+			this.name = "dog";
+		}
+		sayName() {
+			console.log("I am a " + this.name);
+		}
 
-var test=new Test();
-
-//test.fun();
-console.log();
-//实验结果
-//this 依然是指当前的对象TEST 0
-// TEST 1
-// TEST 2
-// TEST 3
-// TEST 4
-// TEST 5
-// TEST 6
-// TEST 7
-// TEST 8
-// TEST 9
-
-Test.staticFun();
-
-console.log(typeof Test);
-
-class Base{
-	static name="allen";
-
-	static sayName(){
-		
+		//arrow function
+		//当我们使用箭头函数时，函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象。
+		//并不是因为箭头函数内部有绑定this的机制，实际原因是箭头函数根本没有自己的this，
+		//它的this是继承外面的，因此内部的this就是外层代码块的this。
+		intervalSayName() {
+			setInterval(() => {
+				console.log(this.name + ":" + (+new Date()));
+			}, 1000);
+		}
 	}
-}
+
+	var dog = new DogES2015();
+	dog.say();
+	dog.sayName();
+
+	//箭头函数  arrow function
+
+	var fun1 = function (i) { return i + 1; };//ES5 
+	var funArrow = (i) => i + 1;//ES2015
+
+	console.log(funArrow(1));
+
+	let cat = "ken";
+	//let dog='lili';
+	let zoo = { cat, dog };
+
+	//Generator 函数
+
+	function* helloworldGenerator() {
+		yield 'hello';
+		yield 'world';
+		return 'ending';
+	}
+
+	var hw = helloworldGenerator();
+
+	hw.next(); //{value:'hello',done:false}
+	hw.next(); //{value:'world',done:false}
+	hw.next(); //{value:'ending',done:true}
+
+	function* f() {
+		console.log('执行了');
+	}
+
+	var g = f();
+
+	setInterval(function () {
+		console.log(g.next());
+	}, 1000);
+
+
+	// 研究下 类方法里的 递归
+
+	class Test {
+		static getName() {
+			return "allen";
+		}
+		static staticFun() {
+			console.log(this.getName());
+		}
+		fun() {
+			console.log('TEST', this.num++);
+			if (this.num == 10) {
+				return;
+			} else {
+				this.fun();
+			}
+		}
+	}
+
+	var test = new Test();
+
+	//test.fun();
+	console.log();
+	//实验结果
+	//this 依然是指当前的对象TEST 0
+	// TEST 1
+	// TEST 2
+	// TEST 3
+	// TEST 4
+	// TEST 5
+	// TEST 6
+	// TEST 7
+	// TEST 8
+	// TEST 9
+
+	Test.staticFun();
+
+	console.log(typeof Test);
+
+
+});
+
+/**
+ * 变量的解析赋值
+ */
+(function () {
+	const {allen, bob} = { allen: "allen", bob: 'bob' };
+	console.log(allen);
+})();
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# myJavaScript
+这是一个我对JavaScript 得一些学习心得，记录一些代码，方便日后复习和查看。
+
+[GitHub地址](https://github.com/wangYalun/myJavaScript)
+
+### 各目录说明
+######  ES2015 
+- 学习ES2015得一些写法，对ES2015得一些写法得验证，并将其编译成ES5的风格,index.js
+- ES2015的类的创建，继承和ES5的写法的对比 index.js-47
+- 变量的解构赋值
+- ES7中的类的静态变量和方法
+- async.js 异步编程的两种写法（回调函数，promise）
+
+###### ES5
+- [UUID.js](https://github.com/wangYalun/myJavaScript/tree/master/ES5/UUID.js) 用JS生成UUID
+- [ajax.js](https://github.com/wangYalun/myJavaScript/tree/master/ES5/ajax.js) 参考jQuery的API风格，封装 XMLHttpRequest
+- [comet.js](https://github.com/wangYalun/myJavaScript/tree/master/ES5/comet.js) 两种服务器推送前端代码（1.基于HTTP流，2.SSE）
+- [promise.js](https://github.com/wangYalun/myJavaScript/tree/master/ES5/promise.js)用ES5实现promise类
+
+###### algorithm
+
+- [sort](https://github.com/wangYalun/myJavaScript/tree/master/algorithm/UUID.js) 用JS实现各种排序算法（冒泡排序，直接插入排序，选择排序，希尔排序，堆排序，归并排序，快速排序）
+- [wechat-lucky-money](https://github.com/wangYalun/myJavaScript/tree/master/algorithm/wechat-lucky-money.js) 测试微信随机红包算法，讲测试结果绘制图形
+
+###### client
+- 在浏览器端测试一些JS功能，写法，设计模式，以及CSS3
+- [js/animate.js]() 设计模式-策略模式和代理模式
+- [js/design_patterns.js]() 设计模式（策略模式之表单验证，代理模式之图片预加载，观察者模式，命令模式，享元模式之对象池，状态模式之开关按钮，状态模式之文件上传）
+- [js/date-selecter.js]() 用观察者模式重构时间选择控件
+- [js/index.js]() 《JavaScript权威指南》客户端的阅读笔记，代码测试（文档加载事件，怪异模式和标准模式注释方法）
+- [css/animation.css]() CSS3 的一些实践（渐变gradient，变型transforms，过渡transitions，动画animation）
+- [css/style.css]() css 中的一些疑点实践
+
+###### data_structures
+> 数据结构的JavaScript 描述
+
+- [List.js]() 列表
+- [Stack.js]() 堆栈
+- [Set.js]() 集合
+- [Queue.js]() 队列
+- [LinkedList.js]() 链表
+- [Graph.js]() 图
+- [Dictionary.js]() 字典
+- [BST.js]() 二叉搜索树
+
+###### design_patterns
+> JavaScript 设计模式
+
+- 函数节流
+- 分时节流
+- 惰性加载
+- 单例模式（透明单例，代理实现单例，惰性单例）
+- 策略模式：定义一些的算法，把他们一个个封装起来，并且使他们相互替换
+- 迭代器模式，迭代器模式是指提供一种方法顺序访问一个聚合对象的各个元素，而又不需要暴露该对象的内部表示。
+- 发布-订阅模式，又叫观察者模式,它定义对象间的一种一对多的依赖关系，当一个对象的状态发生改变时，所以依赖于它的对象都得到通知。
+- 命令模式
+- 组合模式，例子：扫描文件夹
+- 享元模式
+- 职责链模式，AOP实现职责链
+- 中介者模式
+- 装饰者模式
+- 状态模式
+- JavaScript 中的多态
+
+###### test
+
+###### tools 
+- [Router.js]() 路由实现原理
+
+###### client_check.js 
+> 客户端浏览器检查代码
+
+###### [index.js](), [index2.js](), [test.js]()
+> 《JavaScript权威指南》第六版 ，《JavaScript高级程序设计》阅读笔记
+
