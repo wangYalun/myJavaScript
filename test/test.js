bda5cbac85a8aa8a340fb4a2bf708c87727cd628
yalunwang <326402399@qq.com>
2017-05-31 17:17:10 +0800

    测试

--- test/test.js
+++ test/test.js
@@ -1,14 +1,57 @@
 
+var moment = require('moment');
 
-var start=new Date();
-console.log(+start);
+(function () {
+	var start = new Date();
+	console.log(+start);
 
-for(var i=0,sum=0;i<100000000;i++){
-	sum+=1;
-}
+	for (var i = 0, sum = 0; i < 100000000; i++) {
+		sum += 1;
+	}
 
-var end=new Date();
-console.log(+end);
-console.log(end-start);
+	var end = new Date();
+	console.log(+end);
+	console.log(end - start);
+});
+
+(function () {
+	var arr = [1, 2, 3, 4, 5, 6];
+
+	var filterArr = arr.filter((item) => {
+		return item > 7;
+	});
+	console.log(arr);
+	console.log(filterArr);
+
+	var time = moment("19:00", "HH:mm");
+	console.log(moment().to(time, 'mm'));
+
+});
+
+
+(function () {
+
+
+	var obj = {
+		text: "allen",
+		fun: function () {
+			console.log(this.text);
+		},
+		fun2: () => {
+			console.log(this.text);
+		}
+	}
+	var bigObj = {
+		text: "bob",
+		obj: obj
+	}
+
+	obj.fun();
+	obj.fun2();
+
+	bigObj.obj.fun();
+	bigObj.obj.fun2();
+})();
+
 
 
