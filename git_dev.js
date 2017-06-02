20d15c107f1404c75fadd3f888de5e30945d4e15
王雅伦 <wangyl@udiannet.com>
2017-06-02 17:02:47 +0800

    test

--- git_dev.js
+++ git_dev.js
@@ -1,3 +1,15 @@
 console.log("Hello Branch");
 console.log("Hello Dev~");
 
+
+
+//数组测试
+
+var arr=[
+    {name:"allen",age:20},
+    {name:"bob",age:21},
+    {name:"cici",age:22}
+];
+
+console.log(arr.indexOf(arr[1]));
+
